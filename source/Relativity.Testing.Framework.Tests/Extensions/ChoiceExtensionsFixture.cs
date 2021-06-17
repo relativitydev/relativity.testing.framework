a00ab6d8498e9f77605d8ce57c1a8fdb380b8fe8
Paul LeTang <paul.letang@relativity.com>
2021-06-17 10:46:47 -0700

    [TESTENG-1218] Updated Choice:Parent to be a NamedArtifact and added a field name attribute so it gets mapped back properly. Added ChoiceRequest model, and an extension to convert choice to ChoiceRequest.

---
+++ source/Relativity.Testing.Framework.Tests/Extensions/ChoiceExtensionsFixture.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Relativity.Testing.Framework.Extensions;
+using Relativity.Testing.Framework.Models;
+
+namespace Relativity.Testing.Framework.Tests.Extensions
+{
+	[TestFixture]
+	[TestOf(typeof(ChoiceExtensions))]
+	public class ChoiceExtensionsFixture
+	{
+		[Test]
+		public void ToChoiceRequest_CopiesPropertiesFromChoice()
+		{
+			NamedArtifact field = new NamedArtifact { ArtifactID = 12345 };
+			NamedArtifact objectType = new NamedArtifact { ArtifactID = 12345 };
+			NamedArtifact parent = new NamedArtifact { ArtifactID = 12345 };
+
+			Choice choice = new Choice
+			{
+				Active = true,
+				ArtifactID = 12345,
+				Color = ChoiceColor.Gray,
+				Field = field,
+				Name = "AField",
+				ObjectType = objectType,
+				Order = 1,
+				Parent = parent
+			};
+
+			ChoiceRequest choiceRequest = choice.ToChoiceRequest();
+
+			choiceRequest.ArtifactID.Should().Be(12345);
+			choiceRequest.Color.Should().Be(ChoiceColor.Gray);
+			choiceRequest.Field.Should().Be(field);
+			choiceRequest.Name.Should().Be("AField");
+			choiceRequest.Order.Should().Be(1);
+			choiceRequest.Parent.Should().Be(parent);
+		}
+	}
+}
