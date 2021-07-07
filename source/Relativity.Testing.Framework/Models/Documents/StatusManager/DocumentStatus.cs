46dccf849c67b94d762aaaf2ab72c67cfcf22a12
Jonatan Jakoniuk <jonatan.jakoniuk@relativity.com>
2021-07-07 11:23:01 +0200

    RTF-1254 - Create strategies for DocumentStatusManager

---
+++ source/Relativity.Testing.Framework/Models/Documents/StatusManager/DocumentStatus.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Relativity.Testing.Framework.Models
+{
+	/// <summary>
+	/// Contains data about Imaging status of a Document.
+	/// </summary>
+	public class DocumentStatus
+	{
+		/// <summary>
+		/// Gets or sets the number of images for this Document.
+		/// </summary>
+		public int ImageCount { get; set; }
+
+		/// <summary>
+		/// Gets or sets the <see cref="DocumentError"/> connected to this Document.
+		/// </summary>
+		public DocumentError Error { get; set; }
+
+		/// <summary>
+		/// Gets or sets the collection of <see cref="DocumentWarning"/> connected to this Document.
+		/// </summary>
+		public IList<DocumentWarning> Warnings { get; set; }
+
+		/// <summary>
+		/// Gets or sets the HasImages value of this Document. Possible values are Yes, No, Pending, Error.
+		/// </summary>
+		public HasImagesStatus HasImages { get; set; }
+	}
+}
