0eec39900e60ec081a7c32401a771c33cb28716f
Jonatan Jakoniuk <jonatan.jakoniuk@relativity.com>
2021-07-12 10:17:16 +0200

    RTF-1251 - Add models for Native Type

---
+++ source/Relativity.Testing.Framework/Models/Imaging/Enums/BasicCategory.cs
@@ -0,0 +1,43 @@
+﻿namespace Relativity.Testing.Framework.Models
+{
+	/// <summary>
+	/// Specifies the categories of file types or formats for basic imaging. Used by <see cref="NativeType"/> class.
+	/// </summary>
+	public enum BasicCategory
+	{
+		/// <summary>
+		/// The category for the native type isn’t specified.
+		/// </summary>
+		Other = 0,
+
+		/// <summary>
+		/// The native type is a database.
+		/// </summary>
+		Database = 1,
+
+		/// <summary>
+		/// The native type is a graphics file.
+		/// </summary>
+		Graphic = 2,
+
+		/// <summary>
+		/// The native type is a multimedia file.
+		/// </summary>
+		Multimedia = 3,
+
+		/// <summary>
+		/// The native type is a spreadsheet.
+		/// </summary>
+		Spreadsheet = 4,
+
+		/// <summary>
+		/// The native type is a document.
+		/// </summary>
+		WordProcessor = 5,
+
+		/// <summary>
+		/// The native type can't be mapped.
+		/// </summary>
+		Unknown
+	}
+}
---
+++ source/Relativity.Testing.Framework/Models/Imaging/Enums/NativeCategory.cs
@@ -0,0 +1,43 @@
+﻿namespace Relativity.Testing.Framework.Models
+{
+	/// <summary>
+	/// Specifies the categories of file types or formats for native imaging. Used by <see cref="NativeType"/> class.
+	/// </summary>
+	public enum NativeCategory
+	{
+		/// <summary>
+		/// The category for the native type isn’t specified.
+		/// </summary>
+		Other = 0,
+
+		/// <summary>
+		/// The native type is a spreadsheet.
+		/// </summary>
+		Spreadsheet = 1,
+
+		/// <summary>
+		/// The native type is an email message.
+		/// </summary>
+		Email = 2,
+
+		/// <summary>
+		/// The native type is a word processor document.
+		/// </summary>
+		WordProcessor = 3,
+
+		/// <summary>
+		///  The native type is a presentation format, such as Microsoft PowerPoint.
+		/// </summary>
+		Presentation = 4,
+
+		/// <summary>
+		/// The native type is an HTML document.
+		/// </summary>
+		Html = 5,
+
+		/// <summary>
+		/// The native type can't be mapped.
+		/// </summary>
+		Unknown
+	}
+}
