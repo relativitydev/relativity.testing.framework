cd334cdd0f0b40f3b9567c3664d1218df532559c
Paul LeTang <paul.letang@relativity.com>
2021-07-13 12:36:14 -0700

    [TESTENG-1284]/[RTF-1350] Updating Tab Model for v1 API. Also moving FillRequiredProperties to the Tab model.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [4.10.0] - 2021-07-14
+
+### Added
+
+- IsShownInSidebar and IconIdentifier properties to the Tab Model. - [TESTENG-1284](https://jira.kcura.com/browse/TESTENG-1284)
+- Moved FillRequiredProperties to be a method on the Tab Model. - [RTF-1350](https://jira.kcura.com/browse/RTF-1350)
+
 ## [4.9.0] - 2021-07-13
 
 ### Added
---
+++ source/Relativity.Testing.Framework/Models/Tab/TabIconIdentifier.cs
@@ -0,0 +1,53 @@
+﻿using Relativity.Testing.Framework.Attributes;
+
+namespace Relativity.Testing.Framework.Models
+{
+	public enum TabIconIdentifier
+	{
+		[ChoiceName("sidebar-default-tab")]
+		Tab = 1,
+		[ChoiceName("sidebar-access")]
+		Access = 2,
+		[ChoiceName("sidebar-analytics")]
+		Analytics = 3,
+		[ChoiceName("sidebar-bar-chart")]
+		BarChart = 4,
+		[ChoiceName("sidebar-case")]
+		Case = 5,
+		[ChoiceName("sidebar-case-dynamics")]
+		CaseDynamics = 6,
+		[ChoiceName("sidebar-configure")]
+		Configure = 7,
+		[ChoiceName("sidebar-data-transfer")]
+		DataTransfer = 8,
+		[ChoiceName("sidebar-documents")]
+		Documents = 9,
+		[ChoiceName("sidebar-download")]
+		Download = 10,
+		[ChoiceName("sidebar-export")]
+		Export = 11,
+		[ChoiceName("sidebar-folder")]
+		Folder = 12,
+		[ChoiceName("sidebar-infrastructure")]
+		Infrastructure = 13,
+		[ChoiceName("sidebar-monitor")]
+		Monitor = 14,
+		[ChoiceName("sidebar-page")]
+		Page = 15,
+		[ChoiceName("sidebar-pie-chart")]
+		PieChart = 16,
+		[ChoiceName("sidebar-processing")]
+		Processing = 17,
+		[ChoiceName("sidebar-production")]
+		Production = 18,
+		[ChoiceName("sidebar-resources")]
+		Resources = 19,
+		[ChoiceName("sidebar-review")]
+		Review = 20,
+		[ChoiceName("sidebar-upload")]
+		Upload = 21,
+		[ChoiceName("sidebar-workspaces")]
+		Workspaces = 22,
+		None = 0
+	}
+}
