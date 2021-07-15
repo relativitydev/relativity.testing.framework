661ada269e039ab56e82fca60933a22c029c7802
Klaudia Knafel <klaudia.knafel@relativity.com>
2021-07-15 17:39:29 +0200

    [RTF-1253] Adding models for Imaging Job.

---
+++ source/Relativity.Testing.Framework/Models/Imaging/Job/ImagingJobBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Relativity.Testing.Framework.Models
+{
+	/// <summary>
+	/// Base class for Imaging Jobs.
+	/// </summary>
+	public class ImagingJobBase
+	{
+		/// <summary>
+		/// Gets or sets optional unique identifier for the caller of this method. Can be used to differentiate ImagingDocumentRequests
+		/// from different sources.
+		/// </summary>
+		public Guid? OriginationID { get; set; }
+	}
+}
---
+++ source/Relativity.Testing.Framework/Models/Imaging/Job/ImagingJobPriorityRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Relativity.Testing.Framework.Models
+{
+	/// <summary>
+	/// The request object for updating the priority of an imaging job.
+	/// </summary>
+	public class ImagingJobPriorityRequest : ImagingJobBase
+	{
+		/// <summary>
+		/// Gets or sets the new priority.
+		/// </summary>
+		public int Priority { get; set; }
+	}
+}
