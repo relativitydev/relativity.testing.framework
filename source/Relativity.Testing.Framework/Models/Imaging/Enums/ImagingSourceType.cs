bfadc2345ad4f4d977cd6c89cc64bc3b9fc6a404
klaudiaknafel <81292389+klaudiaknafel@users.noreply.github.com>
2021-07-16 14:11:53 +0200

    [RTF-1253] Adding models for Imaging Job. (#58)
    * [RTF-1253] Adding models for Imaging Job.

---
+++ source/Relativity.Testing.Framework/Models/Imaging/Enums/ImagingSourceType.cs
@@ -0,0 +1,24 @@
+﻿namespace Relativity.Testing.Framework.Models
+{
+	/// <summary>
+	/// Determines the document source to be used for creating its Images.
+	/// </summary>
+	public enum ImagingSourceType
+	{
+		/// <summary>
+		/// Document native is used to generate Images.
+		/// </summary>
+		Native = 0,
+
+		/// <summary>
+		/// Document PDF is used to generate Images.
+		/// </summary>
+		RenderedPdf = 1,
+
+		/// <summary>
+		/// Unknown value to which the Imaging Source Type defaults
+		/// if the value does not match any defined value
+		/// </summary>
+		Unknown
+	}
+}
---
+++ source/Relativity.Testing.Framework/Models/Imaging/Job/SingleDocumentImagingJobRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace Relativity.Testing.Framework.Models
+{
+	/// <summary>
+	/// Request used for running a job to image an individual documents.
+	/// </summary>
+	public class SingleDocumentImagingJobRequest : ImagingJobRequest
+	{
+		/// <summary>
+		/// Gets or sets the id of the imaging profile to use.
+		/// </summary>
+		public int ProfileID { get; set; }
+
+		/// <summary>
+		/// Gets or sets optional - location of an alternate native to use while imaging the document
+		/// This file will not be attached to the document, it is only used to generate images.
+		/// </summary>
+		public string AlternateNativeLocation { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether if alternate native files are specified,
+		/// remove them after they are imaged.
+		/// </summary>
+		public bool RemoveAlternateNativeAfterImaging { get; set; }
+	}
+}
