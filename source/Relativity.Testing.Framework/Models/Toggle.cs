70a5c839917bc4830f3628c1c6528664229ad6ff
dmytro-pavliuk <dmytro.pavliuk@relativity.com>
2021-07-28 21:17:02 +0300

    Add toggle model; Update script input.

---
+++ source/Relativity.Testing.Framework/Models/Toggle.cs
@@ -0,0 +1,18 @@
+﻿namespace Relativity.Testing.Framework.Models
+{
+	/// <summary>
+	/// Represents the toggle.
+	/// </summary>
+	public class Toggle
+	{
+		/// <summary>
+		/// Gets or sets name.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether toggle is enabled.
+		/// </summary>
+		public bool IsEnabled { get; set; }
+	}
+}
