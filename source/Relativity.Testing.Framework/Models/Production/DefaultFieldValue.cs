a47d4e48a8c1187b27b6a9ca9837ec576a7f67c0
Jonatan Jakoniuk <jonatan.jakoniuk@relativity.com>
2021-09-08 13:33:57 +0200

    RTF-1263 - Added DefaultFieldValue model

---
+++ source/Relativity.Testing.Framework/Models/Production/DefaultFieldValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Relativity.Testing.Framework.Models
+{
+	/// <summary>
+	/// Represents a default object field value.
+	/// </summary>
+	/// <typeparam name="T">Value type for field.</typeparam>
+	public class DefaultFieldValue<T> : Artifact
+	    where T : NamedArtifact
+	{
+#pragma warning disable CA1720 // Identifier contains type name
+		/// <summary>
+		/// Gets or sets Artifact GUID of field.
+		/// </summary>
+		public Guid Guid { get; set; }
+#pragma warning restore CA1720 // Identifier contains type name
+
+		/// <summary>
+		/// Gets or sets default value for field.
+		/// </summary>
+		public T ChoiceFieldValue { get; set; }
+	}
+}
