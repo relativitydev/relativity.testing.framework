990736f5e641390d0d25cadfdaa4fb316c508207
Maria Kizhaeva <maria.kizhaeva@relativity.com>
2021-09-28 11:25:34 +0200

    [RTF-1262] Production data Source default field values

---
+++ source/Relativity.Testing.Framework/Models/Production/ProductionDataSourceDefaultValues.cs
@@ -0,0 +1,9 @@
+﻿namespace Relativity.Testing.Framework.Models
+{
+	public class ProductionDataSourceDefaultValues
+	{
+		public DefaultFieldValue<NamedArtifact> UseImagePlaceholder { get; set; }
+
+		public DefaultFieldValue<bool> BurnRedactions { get; set; }
+	}
+}
