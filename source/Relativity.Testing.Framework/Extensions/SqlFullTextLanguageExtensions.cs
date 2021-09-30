6621f3c30ba7f79552d3051ee0d7ac58a3d95c20
Maria Kizhaeva <maria.kizhaeva@relativity.com>
2021-09-30 12:37:37 +0200

    [RTF-1401] SqlFullTextLanguage field type changed to enum

---
+++ source/Relativity.Testing.Framework/Extensions/SqlFullTextLanguageExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using Relativity.Testing.Framework.Models;
+
+namespace Relativity.Testing.Framework.Extensions
+{
+	public static class SqlFullTextLanguageExtensions
+	{
+		private static Dictionary<string, SqlFullTextLanguage> _sqlFullTextLanguageDescriptionMap;
+
+		static SqlFullTextLanguageExtensions()
+		{
+			_sqlFullTextLanguageDescriptionMap = new Dictionary<string, SqlFullTextLanguage>();
+			foreach (SqlFullTextLanguage lang in Enum.GetValues(typeof(SqlFullTextLanguage)))
+			{
+				_sqlFullTextLanguageDescriptionMap.Add(lang.ToDescription(), lang);
+			}
+		}
+
+		public static string ToDescription(this SqlFullTextLanguage value)
+		{
+			FieldInfo field = value.GetType().GetField(value.ToString());
+			DescriptionAttribute attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+			return attribute == null ? value.ToString() : attribute.Description;
+		}
+
+		public static SqlFullTextLanguage FromDescription(string langName)
+		{
+			if (!_sqlFullTextLanguageDescriptionMap.ContainsKey(langName))
+			{
+				throw new InvalidEnumArgumentException("Language name not found.");
+			}
+
+			SqlFullTextLanguage result;
+			_sqlFullTextLanguageDescriptionMap.TryGetValue(langName, out result);
+			return result;
+		}
+	}
+}
---
+++ source/Relativity.Testing.Framework/Models/SqlFullTextLanguage.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel;
+
+namespace Relativity.Testing.Framework.Models
+{
+	public enum SqlFullTextLanguage
+	{
+		Neutral = 0,
+		Arabic = 1025,
+
+		[Description("Bengali (India)")]
+		Bengali = 1093,
+
+		Bulgarian = 1026,
+		Brazilian = 1046,
+
+		[Description("British English")]
+		BritishEnglish = 2057,
+
+		Catalan = 1027,
+
+		[Description("Chinese (Hong Kong SAR, PRC)")]
+		Chinese = 3076,
+
+		[Description("Chinese (Macao SAR)")]
+		ChineseMacao = 5124,
+
+		[Description("Chinese (Singapore)")]
+		ChineseSingapore = 4100,
+
+		Croatian = 1050,
+		Czech = 1029,
+		Danish = 1030,
+		Dutch = 1043,
+		English = 1033,
+		French = 1036,
+		German = 1031,
+		Greek = 1032,
+		Gujarati = 1095,
+		Hebrew = 1037,
+		Hindi = 1081,
+		Icelandic = 1039,
+		Indonesian = 1057,
+		Italian = 1040,
+		Japanese = 1041,
+		Kannada = 1099,
+		Korean = 1042,
+		Latvian = 1062,
+		Lithuanian = 1063,
+
+		[Description("Malay - Malaysia")]
+		Malay = 1086,
+
+		Malayalam = 1100,
+		Marathi = 1102,
+
+		[Description("Bokmål")]
+		Norwegian = 1044,
+
+		Polish = 1045,
+		Portuguese = 2070,
+		Punjabi = 1094,
+		Romanian = 1048,
+		Russian = 1049,
+
+		[Description("Serbian (Cyrillic)")]
+		SerbianCyrillic = 3098,
+
+		[Description("Serbian (Latin)")]
+		SerbianLatin = 2074,
+
+		[Description("Simplified Chinese")]
+		SimplifiedChinese = 2052,
+
+		Slovak = 1051,
+		Slovenian = 1060,
+		Spanish = 3082,
+		Swedish = 1053,
+		Tamil = 1097,
+		Telugu = 1098,
+		Thai = 1054,
+
+		[Description("Traditional Chinese")]
+		TraditionalChinese = 1028,
+
+		Turkish = 1055,
+		Ukranian = 1058,
+		Urdu = 1056,
+		Vietnamese = 1066
+	}
+}
