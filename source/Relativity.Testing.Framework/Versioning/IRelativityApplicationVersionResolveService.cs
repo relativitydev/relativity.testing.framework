36eb6966875e6077e3f83f7ffda11f297eb1066e
Peter Azra <peter.azra@relativity.com>
2022-01-26 08:26:26 -0600

    REL-630591 Add method for getting RAP version by guid

--- source/Relativity.Testing.Framework/Versioning/IRelativityApplicationVersionResolveService.cs
+++ source/Relativity.Testing.Framework/Versioning/IRelativityApplicationVersionResolveService.cs
@@ -1,4 +1,6 @@
-﻿namespace Relativity.Testing.Framework.Versioning
+﻿using System;
+
+namespace Relativity.Testing.Framework.Versioning
 {
 	/// <summary>
 	/// Represents the service that resolves the version of a Relativity Application.
@@ -6,10 +8,17 @@
 	public interface IRelativityApplicationVersionResolveService
 	{
 		/// <summary>
-		/// Gets the Application version string.
+		/// Gets the RAP version.
 		/// </summary>
 		/// <param name="appVersionInfo">Object containing identifying info for the Relativity Application and version range.</param>
 		/// <returns>Current version of the RAP installed on Relativity.</returns>
 		string GetVersion(ApplicationVersionRangeAttribute appVersionInfo);
+
+		/// <summary>
+		/// Gets the RAP version.
+		/// </summary>
+		/// <param name="rapGuid">The RAP Guid.</param>
+		/// <returns>Current version of the RAP installed on Relativity.</returns>
+		string GetVersion(Guid rapGuid);
 	}
 }
