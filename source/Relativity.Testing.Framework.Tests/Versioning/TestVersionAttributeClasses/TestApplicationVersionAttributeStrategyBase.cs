ad85e58e0185772a6898deb79963a0edc612124e
Peter Azra <peter.azra@relativity.com>
2022-01-27 10:41:14 -0600

    REL-630591 Update tests for VersionResolveService

---
+++ source/Relativity.Testing.Framework.Tests/Versioning/TestVersionAttributeClasses/TestApplicationVersionAttributeStrategyBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Relativity.Testing.Framework.Tests.Versioning.TestVersionAttributeClasses
+{
+	public abstract class TestApplicationVersionAttributeStrategyBase : ITestApplicationVersionAttributeStrategy
+	{
+		public abstract void DoNothing();
+	}
+}
