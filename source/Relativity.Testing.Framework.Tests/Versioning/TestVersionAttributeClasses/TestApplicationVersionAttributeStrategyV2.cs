c0b2c10c23f0cc024ccdedd54d34281c15e52a79
pazra <60019195+pazra@users.noreply.github.com>
2022-01-27 11:53:10 -0600

    REL-630591 Fixed issue with VersionResolveService handling ApplicationVersionRangeAttribute (#133)
    * REL-630591 Fixed issue with VersionResolveService handling ApplicationVersionRangeAttribute
    
    * REL-630591 Update tests for VersionResolveService

--- source/Relativity.Testing.Framework.Tests/Versioning/TestVersionAttributeClasses/TestApplicationVersionAttributeStrategyV2.cs
+++ source/Relativity.Testing.Framework.Tests/Versioning/TestVersionAttributeClasses/TestApplicationVersionAttributeStrategyV2.cs
@@ -4,9 +4,9 @@ using Relativity.Testing.Framework.Versioning;
 namespace Relativity.Testing.Framework.Tests.Versioning.TestVersionAttributeClasses
 {
 	[ApplicationVersionRange("227ee259-6ce6-4b9c-bbdd-fc3c0b78f275", ">=2.0.0")]
-	internal class TestApplicationVersionAttributeStrategyV2 : ITestApplicationVersionAttributeStrategy
+	internal class TestApplicationVersionAttributeStrategyV2 : TestApplicationVersionAttributeStrategyBase
 	{
-		public void DoNothing()
+		public override void DoNothing()
 		{
 			throw new NotImplementedException();
 		}
