36be02e50cdd646ea8d43ee36fc1adda3136e5f8
AJ Scudiero <aj.scudiero@relativity.com>
2023-02-21 09:32:09 -0600

    RTF-1436 add unit test

---
+++ source/Relativity.Testing.Framework.Tests/Mapping/ObjectMappingServiceFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using Relativity.Testing.Framework.Mapping;
+using Relativity.Testing.Framework.Tests.Models.Mapping;
+
+namespace Relativity.Testing.Framework.Tests.Mapping
+{
+	[TestFixture]
+	[TestOf(typeof(ObjectMappingService))]
+	public class ObjectMappingServiceFixture
+	{
+		[Test]
+		public void Map_MapsDoubleValue_OntoDecimalProperty()
+		{
+			var sut = new ObjectMappingService();
+
+			double testDoubleValue = 5.25;
+
+			var testPropertiesMap = new Dictionary<string, object>();
+
+			testPropertiesMap.Add("TestProperty", testDoubleValue);
+
+			var testObject = new ObjectMappingServiceFixtureTestObject();
+
+			sut.Map(testPropertiesMap, testObject, MappingOptions.Default);
+
+			decimal expectedDecimalValue = new decimal(testDoubleValue);
+
+			testObject.TestProperty.Should().Be(expectedDecimalValue);
+		}
+	}
+}
